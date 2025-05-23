9858d6a9570aae95398cea66e28f382d4b72c2b1
jwhite3107 <jwhite3107@gmail.com>
2025-05-23 12:02:24 +0200

    Delete signal_acquisition.py
    Este archivo no sirve así que lo elimino

--- signal_acquisition.py
+++
@@ -1,9 +0,0 @@
-from bitalino import BITalino
-
-mac_address = "30:16:12:21:98:74"  # Sustituye por la MAC real
-device = BITalino(mac_address)
-
-device.start(samplingRate=10, channels=[0])
-data = device.read(100)
-device.stop()
-device.close()
